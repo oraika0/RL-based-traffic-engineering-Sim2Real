3fe75fd4450fea844b45c6a21de338ed9e793681
oraika0 <li.raika0@gmail.com>
2026-01-03 17:31:54 +0800

    fix config

--- config/controller/simple_monitor_config.py
+++ config/controller/simple_monitor_config.py
@@ -1,15 +1,24 @@
-import os
-home = os.path.expanduser("~")
-cwd = os.getcwd()
+from pathlib import Path
+import os, pwd
+
+def get_user_home():
+    sudo_user = os.environ.get("SUDO_USER")
+    if sudo_user:
+        return Path(pwd.getpwnam(sudo_user).pw_dir)
+    return Path.home()
+
+def find_repo_root(start: Path):
+    for p in [start] + list(start.parents):
+        if (p / "run_drl.py").exists():
+            return p
+    raise RuntimeError("Cannot find repo root")
+
+BASE_DIR = find_repo_root(Path(__file__).resolve())
+USER_HOME = get_user_home()
+
 config = {
-    "conda_sh": home+"/miniconda3/etc/profile.d/conda.sh",
+    "conda_sh": str(USER_HOME / "miniconda3" / "etc" / "profile.d" / "conda.sh"),
     "conda_env": "sdn1",
-    "controller_dir": cwd+"/refactor/utils",
-    "controller_entry": "simple_monitor.py"
+    "controller_dir": str(BASE_DIR / "utils"),
+    "controller_entry": "simple_monitor.py",
 }
-# config = {
-#     "conda_sh": "/home/dcnlab/miniconda3/etc/profile.d/conda.sh",
-#     "conda_env": "sdn1",
-#     "controller_dir": "/home/dcnlab/refactor/utils",
-#     "controller_entry": "simple_monitor.py"
-# }
